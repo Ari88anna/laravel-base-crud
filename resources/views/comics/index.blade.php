3f9fb017ee45f547ec162e38f9a56f444f7a5702
Arianna Carotti <arianna.carotti@gmail.com>
2021-06-15 16:47:58 +0200

    stampati fumetti

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,31 @@
 @extends ('layouts.app')
+
+
+@section('main_content')
+
+    <section>
+        <div class="container">
+
+            <div class="row">
+
+                @foreach($comics as $comic)  
+
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ $comic->image }}" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ $comic->title }}</h5>
+                          <p class="card-text">{{ $comic->description }}</p>
+                          <a href="#" class="btn btn-primary">Dettagli</a>
+                        </div>
+                      </div>
+
+                @endforeach              
+
+            </div>
+
+              
+
+        </div>
+    </section>
+    
+@endsection
